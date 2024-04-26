5cae5bd41ebe72c824a178ef1d56b66ca3e86fa2
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-04-26 17:40:08 +0800

    header styling and positioning done, heade 0.7

--- my-app/app/ui/header.tsx
+++ my-app/app/ui/header.tsx
@@ -10,7 +10,19 @@ export default function Header() {
       <div className={styles.header_contacts}>
         <span className={styles.header_phone}>+7(495)111-11-11</span>
         <span className={styles.header_email}>info@construct.com</span>
+        <button className={styles.header_button}>
+          <img className={styles.header_icon} src='/icons/search.png'/>
+        </button>
       </div>
+      <nav className={styles.header_nav}>
+        <ul>
+            <li><a href="#">ГЛАВНАЯ</a></li>
+            <li><a href="#">О КОМПАНИИ</a></li>
+            <li><a href="#">УСЛУГИ</a></li>
+            <li><a href="#">НОВОСТИ</a></li>
+            <li><a href="#">КОНТАКТЫ</a></li>
+        </ul>
+    </nav>
     </header>
   )
 }
