e2fedd2c385e2948f74b30ef7cace5f03400bce6
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-04-29 20:10:19 +0800

    animation hero done

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Header from './ui/header/header'
 import Hero from "./ui/hero/hero";
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
