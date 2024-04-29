ab456dce96aad646788992dde9a1b896fc43fb4e
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-04-29 18:31:39 +0800

    hero section design done and moving background

--- my-app/app/ui/hero/hero.tsx
+++ my-app/app/ui/hero/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
 
         <h5 className={styles.hero_subname}>СТРОИТЕЛЬНАЯ ГРУППА</h5>
         <h1 className={styles.hero_name}>ТЕРРИТОРИЯ <span className={styles.hero_span}>СТРОИТЕЛЬСТВА</span></h1>
-        {/* <h5 className={styles.hero_moto}>СИСТЕМА ОПТИМАЛЬНЫХ <span className={styles.hero_span}>СТРОИТЕЛЬНЫХ РЕШЕНИЙ</span></h5> */}
+        <button className={styles.hero_button}>ПРЕЗЕНТАЦИЯ КОМПАНИИ</button>
     </section>
   )
 }
