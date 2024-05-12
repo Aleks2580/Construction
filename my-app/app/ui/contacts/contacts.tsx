00b1a91993ae4e51b484abc46cac3c133913b27e
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-05-12 20:19:12 +0800

    form added

--- my-app/app/ui/contacts/contacts.tsx
+++ my-app/app/ui/contacts/contacts.tsx
@@ -1,41 +1,46 @@
-'use client'
+"use client";
 import styles from "./contacts.module.css";
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 export default function Contacts() {
-  // Randomly generating global coordinates
-  const randomLat = Math.random() * 180 - 90; // Latitude range from -90 to 90
-  const randomLng = Math.random() * 360 - 180; // Longitude range from -180 to 180
+  // Coordinates for Tverskaya Street, Moscow
   const mapData = {
-    center: [randomLat, randomLng],
-    zoom: 5, // Zoom level adjusted for broader geographic view
+    center: [55.763285, 37.605993], // Directly using the provided coordinates
+    zoom: 15, // Zoom level adjusted for street view
   };
 
   return (
     <div className={styles.contacts_main}>
-      <div className={styles.contacts_map}>
-        <YMaps>
-          <div>
-            My awesome application with maps!
-            <Map defaultState={mapData} width="100%" height="400px">
-              <Placemark geometry={[randomLat, randomLng]} />
-            </Map>
-          </div>
-        </YMaps>
-      </div>
+      <form action="" className={styles.form}>
+        <h4>ОТПРАВИТЬ СООБЩЕНИЕ</h4>
+        <p>Мы нигде не публикуем ваш e-mail адрес</p>
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <textarea name=""></textarea>
+        <button>ОТПРАВИТЬ</button>
+      </form>
       <div className={styles.contacts_info}>
         <div className={styles.contact_card}>
-          <h3>Phone</h3>
+          <h4>Телефон:</h4>
           <p>+123 456 7890</p>
         </div>
         <div className={styles.contact_card}>
-          <h3>Email</h3>
+          <h4>Email:</h4>
           <p>contact@example.com</p>
         </div>
         <div className={styles.contact_card}>
-          <h3>Address</h3>
-          <p>1234 Street, City, Country</p>
+          <h4>Адрес:</h4>
+          <p>Москва, пр-кт Андропова, д.10, пом.140</p>
         </div>
+      </div>
+      <div className={styles.contacts_map}>
+        <YMaps>
+          <Map className={styles.map} defaultState={mapData}>
+            <Placemark geometry={mapData.center} />
+          </Map>
+        </YMaps>
       </div>
     </div>
   );
