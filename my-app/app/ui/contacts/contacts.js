d48dde35430e456f4f4b6e91c263fb3948bf7f66
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-06-10 21:57:04 +0800

    all done

--- my-app/app/ui/contacts/contacts.js
+++ my-app/app/ui/contacts/contacts.js
@@ -1,6 +1,9 @@
 "use client";
 import styles from "./contacts.module.css";
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { message } from "antd";
 
 const mapData = {
   center: [55.676108, 37.625037],
@@ -8,18 +11,74 @@ const mapData = {
 };
 
 export default function Contacts() {
+  const formRef = useRef(null);
+  const [form, setFrom] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFrom({ ...form, [name]: value });
+  };
+
+  const handleTextareaChange = (e) => {
+    const { name, value } = e.target;
+    setFrom({ ...form, [name]: value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!form.name || !form.email || !form.message) {
+      messageApi.open({
+        type: "warning",
+        content: "Заполните все поля",
+      });
+    } else {
+      setLoading(true);
+      emailjs
+        .send(
+          "service_k382w78",
+          "template_oeks1k8",
+          {
+            from_name: form.name,
+            to_name: "Территория Строительства",
+            from_email: form.email,
+            to_email: "ID@terstroy.com",
+            message: [form.message, `email:${form.email}`],
+          },
+          "88J_75uEmDIBW3VJZ"
+        )
+        .then(
+          () => {
+            setLoading(false);
+            messageApi.open({
+              type: "success",
+              content: "Спасибо! Мы скоро с вами свяжемся.",
+            });
+            setFrom({ name: "", email: "", message: "" });
+          },
+          (error) => {
+            setLoading(false);
+            messageApi.open({
+              type: "error",
+              content: "Что-то пошло не так.",
+            });
+          }
+        );
+    }
+  };
   return (
 <div className={styles.contacts_main}>
+{contextHolder}
   <div className={styles.form_and_cards_section}>
-    <form className={styles.form}>
+    <form className={styles.form} ref={formRef} onSubmit={handleSubmit}>
       <h4 className={styles.form_title}>ОТПРАВИТЬ СООБЩЕНИЕ</h4>
       <p className={styles.form_info}>Мы нигде не публикуем ваш e-mail адрес</p>
       <div className={styles.form_inputs}>
-        <input type="text" placeholder="Ваше имя" className={styles.input} />
-        <input type="email" placeholder="Ваш e-mail" className={styles.input} />
+        <input type="text" placeholder="Ваше имя" className={styles.input} name="name" onChange={handleChange} />
+        <input type="email" placeholder="Ваш e-mail" className={styles.input} name="email" onChange={handleChange}/>
       </div>
-      <textarea placeholder="Ваше сообщение" className={styles.textarea}></textarea>
-      <button type="submit" className={styles.button}>ОТПРАВИТЬ</button>
+      <textarea placeholder="Ваше сообщение" className={styles.textarea} name="message" value={form.message} onChange={handleTextareaChange}></textarea>
+      <button type="submit" className={styles.button}>{loading ? "ОТПРАВКА..." : "ОТПРАВИТЬ"}</button>
     </form>
     <div className={styles.cards_section}>
         <a href="tel:+7(927)115-84-36" className={styles.contact_card}>
@@ -27,9 +86,9 @@ export default function Contacts() {
         <p>+7(927)115-84-36</p>
       </a>
  
-      <a href="mailto:admin@terstroy.com" className={styles.contact_card}>
+      <a href="mailto:ID@terstroy.com" className={styles.contact_card}>
         <img className={styles.icon} src="icons/email.png" alt="email icon" />
-        <p>admin@terstroy.com</p>
+        <p>ID@terstroy.com</p>
       </a>
       <a href="https://yandex.ru/maps/213/moscow/house/varshavskoye_shosse_42/Z04YcAVlSUYAQFtvfXp2d31jbA==/?ll=37.625037%2C55.676108&z=17" target='blank' className={styles.contact_card}>
         <img className={styles.icon} src="icons/location1.png" alt="location icon" />
--- my-app/app/ui/header/header.js
+++ my-app/app/ui/header/header.js
@@ -19,7 +19,7 @@ export default function Header() {
       <Link className={styles.header_logo} href="/" onClick={() => setActive('')}><img className={styles.logo} src="logos/logo-black.png" alt="Company Logo" /></Link>
       <div className={styles.header_contacts}>
         <a className={styles.link} href="tel:+7(927)115-84-36"><span className={styles.header_phone}>+7(927)115-84-36</span></a>
-        <a className={styles.link} href="mailto:admin@terstroy.com"><span className={styles.header_email}>admin@terstroy.com</span></a>
+        <a className={styles.link} href="mailto:ID@terstroy.com"><span className={styles.header_email}>ID@terstroy.com</span></a>
         <button className={styles.header_button}>
           <a className={styles.link} href="https://yandex.ru/maps/213/moscow/house/varshavskoye_shosse_42/Z04YcAVlSUYAQFtvfXp2d31jbA==/?ll=37.625037%2C55.676108&z=17" target='blank'><img className={styles.header_icon} src='/icons/location.png'/></a>
         </button>
